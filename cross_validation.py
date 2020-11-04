c11478457cf93c7dd234d670043524c37b23ff1f
Grzegorz Sarapata <gsarapata@dyn3152-177.wlan.ic.ac.uk>
2020-11-04 15:38:53 +0000

    Merged evaluation and CV

--- cross_validation.py
+++ cross_validation.py
@@ -58,86 +58,86 @@ def grow_binary_trees(data_input, stratified=False, pruning=False):
         return results
 
 
-def get_confusion_matrix(y_true, y_pred, normalised=True):
-    ret_matrix = np.zeros((4, 4))
-    for x, y in zip(y_true, y_pred):
-        ret_matrix[x - 1][y - 1] += 1
+# def get_confusion_matrix(y_true, y_pred, normalised=True):
+#     ret_matrix = np.zeros((4, 4))
+#     for x, y in zip(y_true, y_pred):
+#         ret_matrix[x - 1][y - 1] += 1
 
-    if normalised:
-        return ret_matrix / np.sum(ret_matrix, axis=1).reshape(-1, 1)
-    else:
-        return ret_matrix
+#     if normalised:
+#         return ret_matrix / np.sum(ret_matrix, axis=1).reshape(-1, 1)
+#     else:
+#         return ret_matrix
 
-def get_recalls_precisions(conf_matrix):
+# def get_recalls_precisions(conf_matrix):
 
-    precisions = np.diagonal(conf_matrix) / np.sum(conf_matrix, axis=0)
-    recalls = np.diagonal(conf_matrix) / np.sum(conf_matrix, axis=1)
+#     precisions = np.diagonal(conf_matrix) / np.sum(conf_matrix, axis=0)
+#     recalls = np.diagonal(conf_matrix) / np.sum(conf_matrix, axis=1)
 
-    return precisions, recalls
-
-
-def get_f1_scores(conf_matrix):
-    precisions, recalls = get_recalls_precisions(conf_matrix)
-
-    return 2 * (precisions * recalls) / (precisions + recalls)
-
-def get_class_rate(conf_matrix):
-
-    return np.sum(np.diagonal(conf_matrix)) / np.sum(conf_matrix)
-
-def get_metrics(conf_matrix):
-
-    precision, recall = get_recalls_precisions(conf_matrix)
-    f1 = get_f1_scores(conf_matrix)
-    class_rate = get_class_rate(conf_matrix)
-
-    return precision, recall, f1, class_rate
+#     return precisions, recalls
 
 
-def get_averages(results):
-    '''
-    Returns average metrics from ten folds + half of confidence interval width for each metric.
-    '''
+# def get_f1_scores(conf_matrix):
+#     precisions, recalls = get_recalls_precisions(conf_matrix)
 
-    precisions = []
-    recalls = []
-    f1_scores = []
-    class_rates = []
-    for i in range(len(results)):
-        prec, rec, f1, c_r = get_metrics(results[i])
+#     return 2 * (precisions * recalls) / (precisions + recalls)
 
-        precisions.append(prec)
-        recalls.append(rec)
-        f1_scores.append(f1)
-        class_rates.append(c_r)
-    return {
-        'precision': np.mean(np.array(precisions), axis=0),
-        'precision_CI': np.std(np.array(precisions), ddof=1, axis=0) * 1.96 / np.sqrt(10),
-        'recall': np.mean(np.array(recalls), axis=0),
-        'recall_CI': np.std(np.array(recalls), ddof=1, axis=0) * 1.96 / np.sqrt(10),
-        'f1_score': np.mean(np.array(f1_scores), axis=0),
-        'f1_score_CI': np.std(np.array(f1_scores), ddof=1, axis=0) * 1.96 / np.sqrt(10),
-        'avg_class_rate': np.mean(np.array(class_rates)),
-        'avg_class_rate_CI': np.std(np.array(class_rates), ddof=1) * 1.96 / np.sqrt(10)
-    }
+# def get_class_rate(conf_matrix):
+
+#     return np.sum(np.diagonal(conf_matrix)) / np.sum(conf_matrix)
+
+# def get_metrics(conf_matrix):
+
+#     precision, recall = get_recalls_precisions(conf_matrix)
+#     f1 = get_f1_scores(conf_matrix)
+#     class_rate = get_class_rate(conf_matrix)
+
+#     return precision, recall, f1, class_rate
 
 
-def print_results(results):
-    metrics = get_averages(results)
-    return {
-        'Precision': metrics['precision'],
-        'Precision 95% CI': list(zip(metrics['precision'] - metrics['precision_CI'],
-                                     metrics['precision'] + metrics['precision_CI'])),
-        'Recall': metrics['recall'],
-        'Recall 95% CI': list(zip(metrics['recall'] - metrics['recall_CI'],
-                                  metrics['recall'] + metrics['recall_CI'])),
-        'F1 Score': metrics['recall'],
-        'F1 Score 95% CI': list(zip(metrics['f1_score'] - metrics['f1_score_CI'],
-                                    metrics['f1_score'] + metrics['f1_score_CI'])),
-        'Avg. Class. Rate': metrics['avg_class_rate'],
-        'Avg. Class. Rate 95% CI': (metrics['avg_class_rate'] - metrics['avg_class_rate_CI'],
-                                    metrics['avg_class_rate'] + metrics['avg_class_rate_CI'])
-    }
+# def get_averages(results):
+#     '''
+#     Returns average metrics from ten folds + half of confidence interval width for each metric.
+#     '''
+
+#     precisions = []
+#     recalls = []
+#     f1_scores = []
+#     class_rates = []
+#     for i in range(len(results)):
+#         prec, rec, f1, c_r = get_metrics(results[i])
+
+#         precisions.append(prec)
+#         recalls.append(rec)
+#         f1_scores.append(f1)
+#         class_rates.append(c_r)
+#     return {
+#         'precision': np.mean(np.array(precisions), axis=0),
+#         'precision_CI': np.std(np.array(precisions), ddof=1, axis=0) * 1.96 / np.sqrt(10),
+#         'recall': np.mean(np.array(recalls), axis=0),
+#         'recall_CI': np.std(np.array(recalls), ddof=1, axis=0) * 1.96 / np.sqrt(10),
+#         'f1_score': np.mean(np.array(f1_scores), axis=0),
+#         'f1_score_CI': np.std(np.array(f1_scores), ddof=1, axis=0) * 1.96 / np.sqrt(10),
+#         'avg_class_rate': np.mean(np.array(class_rates)),
+#         'avg_class_rate_CI': np.std(np.array(class_rates), ddof=1) * 1.96 / np.sqrt(10)
+#     }
+
+
+# def print_results(results):
+#     metrics = get_averages(results)
+#     return {
+#         'Precision': metrics['precision'],
+#         'Precision 95% CI': list(zip(metrics['precision'] - metrics['precision_CI'],
+#                                      metrics['precision'] + metrics['precision_CI'])),
+#         'Recall': metrics['recall'],
+#         'Recall 95% CI': list(zip(metrics['recall'] - metrics['recall_CI'],
+#                                   metrics['recall'] + metrics['recall_CI'])),
+#         'F1 Score': metrics['recall'],
+#         'F1 Score 95% CI': list(zip(metrics['f1_score'] - metrics['f1_score_CI'],
+#                                     metrics['f1_score'] + metrics['f1_score_CI'])),
+#         'Avg. Class. Rate': metrics['avg_class_rate'],
+#         'Avg. Class. Rate 95% CI': (metrics['avg_class_rate'] - metrics['avg_class_rate_CI'],
+#                                     metrics['avg_class_rate'] + metrics['avg_class_rate_CI'])
+#     }
 
 
 def metrics_pruning_plot(results_clean, results_clean_pruned, results_noisy, results_noisy_pruned, savefile=False):
