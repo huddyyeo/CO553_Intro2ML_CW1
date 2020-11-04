d35c8b2da76f24d51d3b3048427a225d78c6cd8a
lchoi96 <73135703+lchoi96@users.noreply.github.com>
2020-11-04 08:21:57 +0000

    Updated Graphs

--- graphs.py
+++ graphs.py
@@ -2,14 +2,14 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 data = np.loadtxt('clean_dataset.txt')
-fig, ax = plt.subplots(7, 2, sharex='col', sharey='row')
+fig, ax = plt.subplots(2, 7, sharex='col', sharey='row')
 for i in range(7):
     x1 = data[data[:, -1] == 1][:, i]
     x2 = data[data[:, -1] == 2][:, i]
     x3 = data[data[:, -1] == 3][:, i]
     x4 = data[data[:, -1] == 4][:, i]
     bins_list = np.arange(min(data[:, i]), max(data[:, i]), step=1)
-    ax[i, 0].hist([x1, x2, x3, x4], bins_list, stacked=True, density=True, rwidth=0.8)
+    ax[0, i].hist([x1, x2, x3, x4], bins_list, stacked=True, density=True, rwidth=0.8, fontsize=18)
     # orientation="horizontal"
 data = np.loadtxt('noisy_dataset.txt')
 for i in range(7):
@@ -18,7 +18,6 @@ for i in range(7):
     x3 = data[data[:, -1] == 3][:, i]
     x4 = data[data[:, -1] == 4][:, i]
     bins_list = np.arange(min(data[:, i]), max(data[:, i]), step=1)
-    ax[i, 1].hist([x1, x2, x3, x4], bins_list, stacked=True, density=True, rwidth=0.8)
-
+    ax[1, i].hist([x1, x2, x3, x4], bins_list, stacked=True, density=True, rwidth=0.8, fontsize=18)
 fig.legend(['Room 1', 'Room 2', 'Room 3', 'Room 4'], loc='upper right', fontsize='x-small')
 fig
